d0a3166387b086bd81822521a2c588631a212847
Bslm61 <oussamabouslimane61@gmail.com>
2025-10-09 16:22:07 +0100

    challenge

--- exercices.js
+++ exercices.js
@@ -269,35 +269,67 @@
 
 
 // ------------------Calculator------------------
-var nombre1
-var nombre2 
-var operation 
-var result
-function Calculator(nombre1,operation,nombre2) {
-    switch (operation) {
-        case "+"  :
-            result = nombre1 + nombre2 
-            break;
-        case "-" :
-            result = nombre1 - nombre2
-            break;
-        case "*" : 
-            result = nombre1 * nombre2
-            break;
-        case "/" :
-            result = nombre1 / nombre2
-            break;
-        case "c":  
-            result = (nombre1>nombre2 ?`${nombre1} superieur à ${nombre2}`:`${nombre1} inferieur à ${nombre2}` )
-             break;
-        case "%" :
-            result = nombre1 % nombre2 
-            break; 
+// var nombre1
+// var nombre2 
+// var operation 
+// var result
+// function Calculator(nombre1,operation,nombre2) {
+//     switch (operation) {
+//         case "+"  :
+//             result = nombre1 + nombre2 
+//             break;
+//         case "-" :
+//             result = nombre1 - nombre2
+//             break;
+//         case "*" : 
+//             result = nombre1 * nombre2
+//             break;
+//         case "/" :
+//             result = nombre1 / nombre2
+//             break;
+//         case "c":  
+//             result = (nombre1>nombre2 ?`${nombre1} superieur à ${nombre2}`:`${nombre1} inferieur à ${nombre2}` )
+//              break;
+//         case "%" :
+//             result = nombre1 % nombre2 
+//             break; 
 
-        default:console.log("Please enter a valid number")
-            break;
+//         default:console.log("Please enter a valid number")
+//             break;
     
-    }{
-        return result
-    }
-}console.log(Calculator(2,"%",2))
+//     }{
+//         return result
+//     }
+// // }console.log(Calculator(2,"+",2))
+
+// alert(Calculator(2,"+",2))
+
+// -----------------------Object--------------------------------
+
+// var Students={
+//     name : "Mohammed",
+//     age : 19,
+//     gender: "M",
+//     gpa : 18 ,
+//     height : 186,
+//     weight : 75,
+//     Materiels : {
+//         Serie : "A",
+//         Sn  : 1234   
+//     } 
+// }
+// console.log(Students.Materiels.Sn);
+
+
+// ---------------challenge--------------
+
+
+// var Profil = {
+//     firstname : "Oussama",
+//     lastname : "Bouslimane",
+//     age : 25,
+// get laptop(){
+//   return "lenovo"
+//     }
+// }
+// console.log(Profil.laptop)
